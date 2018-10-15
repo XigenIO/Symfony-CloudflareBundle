ad2e21aef80b4236c5f365b04e1e1d747a181d57
Chris Hilsdon <chrish@xigen.co.uk>
2018-10-15 16:53:34 +0100

    Added methods to make calls to clear edge caches

--- Service/Cloudflare.php
+++ Service/Cloudflare.php
@@ -63,4 +63,64 @@ class Cloudflare
     {
         return new Endpoints\Zones($this->adapter);
     }
+
+    public function getPageRules()
+    {
+        return new Endpoints\PageRules($this->adapter);
+    }
+
+    public function fetchZoneId($domain): ?string
+    {
+        $zones = $this->getZones();
+        foreach ($zones->listZones()->result as $zone) {
+            if ($domain === $zone->name) {
+                return (string) $zone->id;
+            }
+        }
+
+        return false;
+    }
+
+    public function clearCache($zone, array $files = []): bool
+    {
+        $zones = $this->getZones();
+
+        if ([] === $files) {
+            return $zones->cachePurgeEverything($zone);
+        }
+
+        if (count($files) <= 30) {
+            return $zones->cachePurge($zone, $files);
+        }
+
+        $this->clearRemainingFiles($zone, $files);
+
+        return true;
+    }
+
+    private function clearRemainingFiles($zone, array $files = []): bool
+    {
+        $clearing = [];
+        for ($i = 0; $i < 30; $i++) {
+            $result = array_shift($files);
+            if (is_null($result)) {
+                break;
+            }
+
+            $clearing[] = $result;
+        }
+
+        if ([] === $clearing) {
+            return true;
+        }
+
+        $zones = $this->getZones();
+        $zones->cachePurge($zone, $clearing);
+
+        if (count($files) !== 0) {
+            $this->clearCache($zone, $files);
+        }
+
+        return true;
+    }
 }
